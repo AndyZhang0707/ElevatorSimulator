f10f365badd1ccfce81fc5f12be375726e89c61b
Andy Zhang <AndyZhang0707@users.noreply.github.com>
2017-02-07 00:11:28 +0800

    #1 Add JavaDoc to the Main

--- src/io/zhang/andy/Main.java
+++ src/io/zhang/andy/Main.java
@@ -1,10 +1,13 @@
 package io.zhang.andy;
 
+/**
+ * @{link Main} is the project entry point
+ *
+ *        TODO need more information about the project
+ */
 public class Main {
 
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
-
     }
 
 }
